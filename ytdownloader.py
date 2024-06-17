4f0a99533ccf8d4c61b48dc74186329fb6d56b5e
Utkarsha Gupta <108625371+Utkarsha88@users.noreply.github.com>
2024-06-17 10:01:23 +0545

    Update ytdownloader.py
    Download your favorite videos from youtube

--- ytdownloader.py
+++ ytdownloader.py
@@ -8,13 +8,8 @@ try:
     
     print("Title:", yt.title)
     print("Views:", yt.views)
-
-    
     yd = yt.streams.get_highest_resolution()
-    
-    
     yd.download()
-    
     print("Download complete.")
 except Exception as e:
     print("An error occurred:", str(e))
